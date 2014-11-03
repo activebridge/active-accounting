0aa4cb2608f0bda6f5e93b3043a1c7d42df0b8ae
victor <vitya.shmigol@gmail.com>
2014-11-03 15:58:22 +0200

    fix all problem

--- app/serializers/register_serializer.rb
+++ app/serializers/register_serializer.rb
@@ -1,5 +1,5 @@
 class RegisterSerializer < ActiveModel::Serializer
-  attributes :id, :date, :counterparty, :article, :value, :notes
+  attributes :id, :date, :counterparty, :counterparty_id, :article, :article_id, :value, :notes
 
   def counterparty
     object.counterparty.name if object.counterparty
