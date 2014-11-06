7a7d213e01388737f2a4bbb3c25be0a543fd291d
victor <vitya.shmigol@gmail.com>
2014-11-06 15:12:08 +0200

    fix

--- app/assets/javascripts/jquery-ui/datepicker.js
+++ app/assets/javascripts/jquery-ui/datepicker.js
@@ -74,16 +74,17 @@ function Datepicker() {
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[""] = { // Default regional settings
 		closeText: "Done", // Display text for close link
-		prevText: "Prev", // Display text for previous month link
-		nextText: "Next", // Display text for next month link
+		prevText: "Попередня", // Display text for previous month link
+		nextText: "Наступна", // Display text for next month link
 		currentText: "Today", // Display text for current month link
-		monthNames: ["January","February","March","April","May","June",
-			"July","August","September","October","November","December"], // Names of months for drop-down and formatting
-		monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // For formatting
-		dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // For formatting
-		dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], // For formatting
-		dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"], // Column headings for days starting at Sunday
-		weekHeader: "Wk", // Column header for week of the year
+		monthNames: ['Січень','Лютий','Березень','Квітень','Травень','Червень',
+	'Липень','Серпень','Вересень','Жовтень','Листопад','Грудень'], // Names of months for drop-down and formatting
+		monthNamesShort: ['Січ','Лют','Бер','Кві','Тра','Чер',
+	'Лип','Сер','Вер','Жов','Лис','Гру'], // For formatting
+		dayNames: ['неділя','понеділок','вівторок','середа','четвер','п’ятниця','субота'], // For formatting
+		dayNamesShort: ['нед','пнд','вів','срд','чтв','птн','сбт'], // For formatting
+		dayNamesMin: ['Нд','Пн','Вт','Ср','Чт','Пт','Сб'], // Column headings for days starting at Sunday
+		weekHeader: "Тиж", // Column header for week of the year
 		dateFormat: "mm/dd/yy", // See format options on parseDate
 		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
