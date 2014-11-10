537b32f74b73a945c3a6dd4a741272918e112817
Vitaliy Bondarenko <joker-tref@ukr.net>
2014-11-10 10:17:19 +0200

    remove the extra spaces

--- db/migrate/20141105130155_change_active_default_in_counterparties.rb
+++ db/migrate/20141105130155_change_active_default_in_counterparties.rb
@@ -1,5 +1,5 @@
 class ChangeActiveDefaultInCounterparties < ActiveRecord::Migration
   def change
-  	change_column_default :counterparties, :active, true
+    change_column_default :counterparties, :active, true
   end
 end
