06cf651131ba856745c4529d60a3dc8a1bc3c58a
Vitaliy Bondarenko <joker-tref@ukr.net>
2014-11-11 19:22:54 +0200

    add spec on CounterpartiesController (active, inactive counterparties)

--- spec/controllers/counterparties_controller_spec.rb
+++ spec/controllers/counterparties_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe CounterpartiesController, :type => :controller do
 
+  describe "#index" do
+    let!(:activeCounterparty) { FactoryGirl.create(:counterparty) }
+    let!(:inactiveCounterparty) { FactoryGirl.create(:counterparty, active: false) }
+
+    it 'unload only active counterparties' do
+      get :index, scope: 'active', format: :json
+      JSON.parse(response.body).each do |json|
+        json["active"].should == true
+      end
+    end
+
+    it 'unload only inactive counterparties' do
+      get :index, scope: 'inactive', format: :json
+      JSON.parse(response.body).each do |json|
+        json["active"].should == false
+      end
+    end
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+USER = 'developer'
+PASSWORD = 'password'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
@@ -24,6 +26,7 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -47,4 +50,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before do
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(USER, PASSWORD)
+  end
 end
