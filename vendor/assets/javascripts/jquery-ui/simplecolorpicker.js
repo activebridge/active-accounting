7d1774edca7be975b81d09fffc52d489d1e06c11
Vitaliy Bondarenko <joker-tref@ukr.net>
2014-11-26 13:51:20 +0200

    fix problem appearance colorpicker

--- vendor/assets/javascripts/jquery-ui/simplecolorpicker.js
+++ vendor/assets/javascripts/jquery-ui/simplecolorpicker.js
@@ -138,7 +138,7 @@
       var pos = this.$icon.offset();
       this.$picker.css({
         // Remove some pixels to align the picker icon with the icons inside the dropdown
-        left: pos.left - 6,
+        left: pos.left - 70,
         top: pos.top + this.$icon.outerHeight()
       });
 
