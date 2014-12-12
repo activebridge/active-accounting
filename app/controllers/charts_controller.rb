b085eddb99c9b1ebb6ce834d6cdf50a667f8f542
Vitaliy Bondarenko <joker-tref@ukr.net>
2014-12-12 10:18:48 +0200

    loading in charts only data fact

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,13 @@
 class ChartsController < ApplicationController
   def index
-    json = ActiveModel::ArraySerializer.new(Register.by_year(params[:year]).group_by_month,
+    json = ActiveModel::ArraySerializer.new(Fact.by_year(params[:year]).group_by_month,
                                           each_serializer: Charts::GeneralSerializer,
                                           root: nil)
     render json: json, status: 200
   end
 
   def years
-    json = Register.pluck(:date).map(&:year).uniq.sort
+    json = Fact.pluck(:date).map(&:year).uniq.sort
     render json: json, status: 200
   end
 end
