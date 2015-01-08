0d6c277c1a08a66c1d7ec4fd1a4b8dcd30ec835f
Vitaliy Bondarenko <joker-tref@ukr.net>
2015-01-08 15:19:56 +0200

    spec: edit and add

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -26,9 +26,15 @@ RSpec.describe ReportsController, :type => :controller do
                                          value: 20,
                                          date: '12/10/2014') }
 
+    let!(:register_plan) { FactoryGirl.create(:register_plan,
+                                         article: article2,
+                                         counterparty: counterparty2,
+                                         value: 50,
+                                         date: '13/10/2014') }
+
     context 'returns registers for selected months' do
       before do
-        get :index, report_type: "revenues", months: ['10/2014', '11/2014']
+        get :index, report_type: "revenues", months: ['10/2014', '11/2014'], rate_currency: 10
       end
 
       it { expect(json).to eq(
@@ -37,9 +43,11 @@ RSpec.describe ReportsController, :type => :controller do
             {
               'article' => article.name,
               'values' => { '10' => '10.0', '11' => '0' },
+              'valuesPlan' => { '10' => '0', '11' => '0' },
               'counterparties' => [
                 'counterparty' => counterparty.name,
-                'values' => { '10' => '10.0', '11' => '0' }
+                'values' => { '10' => '10.0', '11' => '0' },
+                'valuesPlan' => { '10' => '0', '11' => '0' }
               ],
               'article_type' => 'Revenue',
               'article_id' => article.id
@@ -47,15 +55,18 @@ RSpec.describe ReportsController, :type => :controller do
             {
               'article' => article2.name,
               'values' => { '10' => '20.0', '11' => '25.0' },
+              'valuesPlan' => { '10' => '50.0', '11' => '0' },
               'counterparties' => [
                 'counterparty' => counterparty2.name,
-                'values' => { '10' => '20.0', '11' => '25.0' }
+                'values' => { '10' => '20.0', '11' => '25.0' },
+                'valuesPlan' => { '10' => '50.0', '11' => '0' }
               ],
               'article_type' => 'Revenue',
               'article_id' => article2.id
             }
           ],
-          'total_values' => { '10' => 30.0, '11' => 25.0 }
+          'total_values' => { '10' => 30.0, '11' => 25.0 },
+          'total_values_plan' => { '10' => 50.0 }
         ]
       ) }
     end
