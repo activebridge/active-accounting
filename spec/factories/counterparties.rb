eed53c18bcfad9230232280b005186ccf74dcb98
victir <vitya.shmigol@gmail.com>
2015-04-30 11:03:27 +0300

    add spec

--- spec/factories/counterparties.rb
+++ spec/factories/counterparties.rb
@@ -9,5 +9,10 @@ FactoryGirl.define do
     factory :customer, parent: :counterparty, class: 'Customer' do
       type 'Customer'
     end
+
+    factory :vendor, parent: :counterparty, class: 'Vendor' do
+      type 'Vendor'
+      password { Faker::Internet.password(8) }
+    end
   end
 end
