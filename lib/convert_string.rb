297653c059532cc98e1d6898c6dbd631bcda30ba
victir <vitya.shmigol@gmail.com>
2015-05-28 18:58:24 +0300

    fix hardcode

--- lib/convert_string.rb
+++
@@ -1,7 +0,0 @@
-class ConvertString
-
-  def full_name_to_short_name(full_name)
-    array = full_name.split.each{ |i| i.capitalize! }
-    array[0] + " #{array[1][0]}.#{array[2][0]}."
-  end
-end
--- spec/models/vendor_info_spec.rb
+++ spec/models/vendor_info_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe VendorInfo, type: :model do
+  let(:vendor_info) { FactoryGirl.create(:vendor_info, name: 'Галушка Олександр Анатолійович') }
+
   context "associations" do
     it { should belong_to(:vendor) }
+  end
+
+  it 'convert short_name' do
+    expect(vendor_info.short_name).to eq('Галушка О.А.')
   end
 end
