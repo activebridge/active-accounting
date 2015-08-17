f13744f5e7bcbe89482856f988d38c79a70b2b8b
Oleg Sobchuk <oleg.sobchuk@gmail.com>
2015-08-17 16:39:10 +0300

    change line chart displaying

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require highcharts
 //= require jquery.validate
 //= require jquery.validate.additional-methods
 //= require bootstrap-sprockets
