8153909b7620f7cebaa08dbcefaba0c568c1f00f
alex-pro <a.partitskiy@gmail.com>
2016-02-11 12:46:10 +0200

    payment history

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+  before_filter :set_paper_trail_whodunnit
   respond_to :html, :json
 
   protect_from_forgery with: :exception
@@ -8,4 +9,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     return '#home'
   end
+
+  alias_method :current_user, :current_admin
 end
