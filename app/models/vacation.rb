804cb68b3901c4f08b97d43a94be811b6f156b9e
alex-pro <a.partitskiy@gmail.com>
2016-07-18 11:56:29 +0300

    fixed validation in vacation model

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -2,7 +2,7 @@ class Vacation < ActiveRecord::Base
   belongs_to :vendor
 
   validates :start, :ending, :vendor_id, presence: true
-  validates :start, :ending, uniqueness: true
+  validates :start, :ending, uniqueness: { scope: :vendor_id }
 
   scope :by_year, -> (year) { where('extract(year from start) = ?', year) if year }
 end
--- spec/models/vacations_spec.rb
+++ spec/models/vacations_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Vacation, type: :model do
   context 'associations' do
     it { is_expected.to belong_to(:vendor) }
   end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:start) }
+    it { is_expected.to validate_presence_of(:ending) }
+    it { is_expected.to validate_presence_of(:vendor_id) }
+    it { is_expected.to validate_uniqueness_of(:start).scoped_to(:vendor_id) }
+    it { is_expected.to validate_uniqueness_of(:ending).scoped_to(:vendor_id) }
+  end
 end
